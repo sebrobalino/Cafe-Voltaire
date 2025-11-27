4b3391bf16fa94edfb742bf0115836e5a35b9d00
gtimsen <gtimsen@gmail.com>
2025-11-27 14:52:25 -0500

    Add frontend UI + Firebase config fixes + login screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import LoginScreen from "./LoginScreen";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const Root = () => {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      setLoggedIn(!!user);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return loggedIn ? (
     <App />
-  </React.StrictMode>
-);
+  ) : (
+    <LoginScreen onLogin={() => setLoggedIn(true)} />
+  );
+};
 
+ReactDOM.createRoot(document.getElementById("root")!).render(<Root />);
